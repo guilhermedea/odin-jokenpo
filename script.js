9ef6b265a16fac5c5537cbfa2d67f68ab6adb1c8
Guilherme Dea <guilhermezdea@gmail.com>
2024-06-25 16:38:42 -0300

    Add new divs to the HTML page, moved JS from HTML to script.js file, changed style css to add new colors for UI.

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 const result = document.querySelector("#result-container");
+const finalScoreContainer = document.querySelector("#final-score");
+const finalResultContainer = document.querySelector("#final-result");
+const finalScore = document.createElement ("p");
+const finalResult = document.createElement ("p");
+
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", () => {
+    window.location.href=window.location.href
+})
+
+const rockButton = document.querySelector("#rock");
+rockButton.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+})
+
+const paperButton = document.querySelector("#paper");
+
+paperButton.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+})
+
+const scissorsButton = document.querySelector("#scissors");
+scissorsButton.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+})
+
+
+let humanScore = 0;
+let computerScore = 0;
+let roundNumber = 0;
+
+function showResetButton () {
+    resetButton.style.display = "block";
+}
 
 function getComputerChoice () {
     let number = (Math.floor(Math.random() * 100) + 1);
@@ -30,35 +64,76 @@ function getUserChoice() {
 }
 
 function playRound (humanChoice, computerChoice) {
-    
-    const resultText = document.createElement("p");
 
-        if (humanChoice === computerChoice) {
-            resultText.textContent = "It's a draw!";
-            result.appendChild(resultText);
-        } else if (humanChoice === "rock" && computerChoice === "scissors") {
-            resultText.textContent = "The user wins! Rock beats scissors!";
-            result.appendChild(resultText);
-            humanScore++;
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            resultText.textContent = "The user wins! Paper beats rock!";
-            result.appendChild(resultText);
-            humanScore++;
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            resultText.textContent = "The user wins! Scissors beats paper!";
-            result.appendChild(resultText);
-            humanScore++;
-        } else if (computerChoice === "rock" && humanChoice === "scissors") {
-            resultText.textContent = "The CPU wins! Rock beats scissors!";
-            result.appendChild(resultText);
-            computerScore++;
-        } else if (computerChoice === "paper" && humanChoice === "rock") {
-            resultText.textContent = "The CPU wins! Paper beats rock!";
-            result.appendChild(resultText);
-            computerScore++;
-        } else if (computerChoice === "scissors" && humanChoice === "paper") {
-            resultText.textContent = "The CPU wins! Scissors beats paper!";
-            result.appendChild(resultText);
-            computerScore++;
-        } 
+    if (roundNumber < 5) {
+   
+            const resultText = document.createElement("p");
+
+                if (humanChoice === computerChoice) {
+                    resultText.textContent = "Woooah!, it's a draw! Keep going!";
+                    console.log("It worked!")
+                    result.appendChild(resultText);
+                    result.appendChild(document.createElement("br"));
+                    roundNumber++
+                } else if (humanChoice === "rock" && computerChoice === "scissors") {
+                    resultText.textContent = "The user wins! Rock beats scissors!";
+                    console.log("It worked!")
+                    result.appendChild(resultText);
+                    result.appendChild(document.createElement("br"));
+                    roundNumber++
+                    humanScore++;
+                } else if (humanChoice === "paper" && computerChoice === "rock") {
+                    resultText.textContent = "The user wins! Paper beats rock!";
+                    console.log("It worked!")
+                    result.appendChild(resultText);
+                    result.appendChild(document.createElement("br"));
+                    roundNumber++
+                    humanScore++;
+                } else if (humanChoice === "scissors" && computerChoice === "paper") {
+                    resultText.textContent = "The user wins! Scissors beats paper!";
+                    console.log("It worked!")
+                    result.appendChild(resultText);
+                    result.appendChild(document.createElement("br"));
+                    roundNumber++
+                    humanScore++;
+                } else if (computerChoice === "rock" && humanChoice === "scissors") {
+                    resultText.textContent = "The CPU wins! Rock beats scissors!";
+                    console.log("It worked!")
+                    result.appendChild(resultText);
+                    result.appendChild(document.createElement("br"));
+                    roundNumber++
+                    computerScore++;
+                } else if (computerChoice === "paper" && humanChoice === "rock") {
+                    resultText.textContent = "The CPU wins! Paper beats rock!";
+                    console.log("It worked!")
+                    result.appendChild(resultText);
+                    result.appendChild(document.createElement("br"));
+                    roundNumber++
+                    computerScore++;
+                } else if (computerChoice === "scissors" && humanChoice === "paper") {
+                    resultText.textContent = "The CPU wins! Scissors beats paper!";
+                    console.log("It worked!")
+                    result.appendChild(resultText);
+                    result.appendChild(document.createElement("br"));
+                    roundNumber++
+                    computerScore++;
+                } 
+            } else {
+                finalScore.textContent = `The final score is ${humanScore} x ${computerScore}`;
+                finalScoreContainer.appendChild(finalScore)
+
+                    if (humanScore === computerScore) {
+                    finalResult.textContent = "It's a draw!";
+                    finalResultContainer.appendChild(finalResult);
+                    showResetButton();
+                    } else if (humanScore > computerScore) {
+                        finalResult.textContent = "You win!";
+                        finalResultContainer.appendChild(finalResult);
+                        showResetButton();
+                    } else {
+                        finalResult.textContent = "The CPU Wins!";
+                        finalResultContainer.appendChild(finalResult);
+                        showResetButton();
+                    }
+     }
 }
